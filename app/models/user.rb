d3fbeaf432b10b65600ad9c61a099d3fe4802bd1
leahliu <leahliu7@gmail.com>
2013-01-11 10:04:51 +0800

    log in and sign up

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+     record.errors[attribute] << (options[:message]) || "Please enter an email"
+    end
+  end
+end
+class User < ActiveRecord::Base
+  #has_secure_password
+   attr_accessible :password, :email, :username, :password_confirmation
+   attr_accessor :password
+   before_save :encrypt_password
+   validates :password, :confirmation => true, :on => :create
+   validates:email, :presence => true, :email => true, :uniqueness => true# :email_format => true
+   validates:username, :presence => true, :length => {:minimum => 3}, :uniqueness =>      true
+   validates:password, :presence => true, :length => {:minimum => 6}
+  def self.authenticate(email,password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'b1ab696ec5fea8ac143757dae049f942ecde75b694508c0c6f73e9707d8a248d5399dea54529c4e67f8f12e149e3f34a9892c50dd91177efd8e519c510e0034b'
